9ae0b45a147807a5cd5b0ae6218ac80e00758a43
Wait Really <47409942+RogelioSierra@users.noreply.github.com>
2024-02-09 22:02:37 -0800

    ch

--- bemine.js
+++ bemine.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 document.getElementById('yesBtn').addEventListener('click', function() {
-  document.getElementById('response').innerHTML = '<iframe src="https://giphy.com/embed/8UGGp7rQvfhe63HrFq" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/reaction-mood-8UGGp7rQvfhe63HrFq">via GIPHY</a></p>';
+  document.getElementById('response').innerHTML = '<iframe src="https://giphy.com/embed/2dQ3FMaMFccpi" width="480" height="460" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/love-i-you-that-70s-show-2dQ3FMaMFccpi">via GIPHY</a></p>';
   document.getElementById('response').innerText = "Great! I love you!";
 });
 
