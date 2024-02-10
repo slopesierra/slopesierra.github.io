c31ea115d7f2bda69392d216700f92ad41db642e
Wait Really <47409942+RogelioSierra@users.noreply.github.com>
2024-02-09 21:12:55 -0800

    changed js to fix button not clicking

--- bemine.js
+++ bemine.js
@@ -1,5 +1,5 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
-  document.getElementById('response').innerHTML = '<iframe src="https://giphy.com/embed/8UGGp7rQvfhe63HrFq" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/reaction-mood-8UGGp7rQvfhe63HrFq">via GIPHY</a></p>'
+  document.getElementById('response').innerHTML = '<iframe src="https://giphy.com/embed/8UGGp7rQvfhe63HrFq" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/reaction-mood-8UGGp7rQvfhe63HrFq">via GIPHY</a></p>';
   document.getElementById('response').innerText = "Great! I love you!";
 });
 
