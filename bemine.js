fb07cebf5f6b6a0bdbe08631c6f94dced9199967
Wait Really <47409942+RogelioSierra@users.noreply.github.com>
2024-02-09 20:55:50 -0800

    fixed gif

--- bemine.js
+++ bemine.js
@@ -3,12 +3,12 @@ document.getElementById('yesBtn').addEventListener('click', function() {
   });
   
   document.getElementById('noBtn').addEventListener('click', function() {
-    document.getElementById('response').innerText = "WRONG. GO BACK.";
+    document.getElementById('response').innerText = "WRONG.GO BACK";
     fetchSassyGif();
   });
   
   function fetchSassyGif() {
-    fetch('https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZW9ldWtiamY2b21qdWZsNHFpd2Q5OG4xMnlwMjNndTl5aGZtbjQ3dSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/HlZpXqgejdpUrwroHL/giphy.gif')
+    fetch('https://api.giphy.com/v1/gifs/random?api_key=YOUR_GIPHY_API_KEY&tag=sassy')
       .then(response => response.json())
       .then(data => {
         const gifUrl = data.data.image_original_url;
