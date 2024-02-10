8056940c24fd41d27372ee374735f0b9ec3c39b9
Wait Really <47409942+RogelioSierra@users.noreply.github.com>
2024-02-09 21:24:22 -0800

    did a domc wrap

--- bemine.js
+++ bemine.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+
 document.getElementById('yesBtn').addEventListener('click', function() {
   document.getElementById('response').innerHTML = '<iframe src="https://giphy.com/embed/8UGGp7rQvfhe63HrFq" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/reaction-mood-8UGGp7rQvfhe63HrFq">via GIPHY</a></p>';
   document.getElementById('response').innerText = "Great! I love you!";
@@ -6,4 +8,6 @@ document.getElementById('yesBtn').addEventListener('click', function() {
 document.getElementById('noBtn').addEventListener('click', function() {
   document.getElementById('response').innerHTML = '<iframe src="https://giphy.com/embed/MoYC1N4nv7Fcs" width="480" height="416" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/sassy-spn-gurl-MoYC1N4nv7Fcs">via GIPHY</a></p>';
   document.getElementById('response').innerHTML += "<p>WRONG. TRY AGAIN, LOSER.</p>";
+});
+  // Your JavaScript code here
 });
