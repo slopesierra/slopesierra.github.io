0fa191918a80374e227d2cb7d4cac1f3c6772052
Wait Really <47409942+RogelioSierra@users.noreply.github.com>
2024-02-09 21:08:53 -0800

    changed js

--- bemine.js
+++ bemine.js
@@ -1,24 +1,9 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
-    document.getElementById('response').innerText = "Great! I love you!";
-  });
-  
-  document.getElementById('noBtn').addEventListener('click', function() {
-    document.getElementById('response').innerText = "WRONG.GO BACK";
-    fetchSassyGif();
-  });
-  
-  function fetchSassyGif() {
-    fetch('https://api.giphy.com/v1/gifs/random?api_key=YOUR_GIPHY_API_KEY&tag=sassy')
-      .then(response => response.json())
-      .then(data => {
-        const gifUrl = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZW9ldWtiamY2b21qdWZsNHFpd2Q5OG4xMnlwMjNndTl5aGZtbjQ3dSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/HlZpXqgejdpUrwroHL/giphy.gif';
-        const img = document.createElement('img');
-        img.src = https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZW9ldWtiamY2b21qdWZsNHFpd2Q5OG4xMnlwMjNndTl5aGZtbjQ3dSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/HlZpXqgejdpUrwroHL/giphy.gif;
-        img.alt = 'Sassy GIF';
-        const responseContainer = document.getElementById('response');
-        responseContainer.innerHTML = '';
-        responseContainer.appendChild(img);
-      })
-      .catch(error => console.error('Error fetching sassy gif:', error));
-  }
-  
+  document.getElementById('response').innerHTML = '<iframe src="https://giphy.com/embed/8UGGp7rQvfhe63HrFq" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/reaction-mood-8UGGp7rQvfhe63HrFq">via GIPHY</a></p>'
+  document.getElementById('response').innerText = "Great! I love you!";
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+  document.getElementById('response').innerHTML = '<iframe src="https://giphy.com/embed/MoYC1N4nv7Fcs" width="480" height="416" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/sassy-spn-gurl-MoYC1N4nv7Fcs">via GIPHY</a></p>';
+  document.getElementById('response').innerHTML += "<p>WRONG. TRY AGAIN, LOSER.</p>";
+});
